a1a848f3f76ef8497950dbbf8d57870efec00298
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2025-01-17 03:49:14 +0500

    Appbar + Used stateful widget

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget {
+class CurrencyConverterMaterialPage extends StatefulWidget {
   const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  State createState() => _CurrencyConverterMaterialPageState();
+}
+
+class _CurrencyConverterMaterialPageState extends State {
+  // @override
+  // void initState(){ // called before the build function
+  //   super.initState();
+  // }
+    double result = 0;
+    final TextEditingController textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Hello World!",
-                style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 255, 255, 255),
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        elevation: 20,
+        title: Text(
+          "Currency Converter",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              result.toString(),
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
               ),
-              TextField(
+            ),
+            // padding and container
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+              child: TextField(
+                controller: textEditingController,
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
                 style: TextStyle(color: Colors.black),
                 decoration: InputDecoration(
                   hintText: "Please enter amount in PKR",
@@ -28,10 +57,11 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                   fillColor: Colors.white,
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(
-                        color: Colors.green,
-                        width: 2,
-                        style: BorderStyle.solid,
-                        strokeAlign: BorderSide.strokeAlignOutside),
+                      color: Colors.green,
+                      width: 2,
+                      style: BorderStyle.solid,
+                      strokeAlign: BorderSide.strokeAlignOutside,
+                    ),
                     borderRadius: BorderRadius.horizontal(
                       right: Radius.circular(30),
                     ),
@@ -43,9 +73,60 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState((){
+                      result = double.parse(textEditingController.text) * 278.80;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    minimumSize: Size(
+                      double.infinity,
+                      50,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text("Convert"),
+                )
+                // child: ElevatedButton(
+                //   style: ButtonStyle(
+                //     backgroundColor: WidgetStatePropertyAll(Colors.black),
+                //     foregroundColor: WidgetStatePropertyAll(Colors.white),
+                //     minimumSize: WidgetStatePropertyAll(
+                //       Size(
+                //         double.infinity,
+                //         50,
+                //       ),
+                //     ),
+                //     shape: WidgetStatePropertyAll(
+                //       RoundedRectangleBorder(
+                //         side: BorderSide(
+                //           color: Colors.black,
+                //         ),
+                //         borderRadius: BorderRadius.circular(10),
+                //       ),
+                //     ),
+                //   ),
+                //   onPressed: () => debugPrint("button clicked"),
+                //   child: const Text(
+                //     "Convert",
+                //   ),
+                // ),
+                )
+          ],
         ),
-        backgroundColor: Colors.blueGrey);
+      ),
+      backgroundColor: Colors.blueGrey,
+    );
   }
 }
